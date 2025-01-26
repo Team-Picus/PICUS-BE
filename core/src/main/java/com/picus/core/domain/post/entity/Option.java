f7bdee289d54b71993ee4740d3f39146d6e7b088
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-01-26 19:48:47 +0900

    feat: Post aggregation 설계

--- core/src/main/java/com/picus/core/domain/post/entity/Option.java
+++ core/src/main/java/com/picus/core/domain/post/entity/Option.java
@@ -1,7 +1,18 @@
 package com.picus.core.domain.post.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Option {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "option_id")
+    private Long id;
+
+    @Column(name = "post_no")
+    private Long postId; // post id
+
     private String name;
-    private Price price;
+    private Double price;
 }
