544366c8931df915e95b5076891cadb3d2103edf
koungq <guu119@naver.com>
2025-01-29 20:12:26 +0900

    feat: 예약 내역 캡처 클래스

--- core/src/main/java/com/picus/core/domain/reservation/entity/history/SelectedOption.java
+++ core/src/main/java/com/picus/core/domain/reservation/entity/history/SelectedOption.java
@@ -1,32 +1,21 @@
 package com.picus.core.domain.reservation.entity.history;
 
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Document(collection = "selected_option")
 public class SelectedOption {
 
-    @Id
-    @Field(name = "reservation_no")
-    private Long id;
-
-    // Option
     private Long postNo;
 
     private String name;
 
     private Double price;
 
-    @Field(name = "applied_discounts")
     private List<AppliedDiscount> appliedDiscounts = new ArrayList<>();
 }
