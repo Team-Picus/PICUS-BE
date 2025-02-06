d2ea91b23492dd70d6e2adbd3209b2ccc574ee4a
koungq <guu119@naver.com>
2025-02-06 23:21:48 +0900

    feat: api 응답 클래스

--- core/src/main/java/com/picus/core/domain/expert/entity/Expert.java
+++ core/src/main/java/com/picus/core/domain/expert/entity/Expert.java
@@ -3,7 +3,7 @@ package com.picus.core.domain.expert.entity;
 import com.picus.core.domain.expert.entity.area.ExpertDistrict;
 import com.picus.core.global.common.BaseEntity;
 import com.picus.core.global.common.enums.ApprovalStatus;
-import com.picus.core.global.converter.StringSetConverter;
+import com.picus.core.global.common.converter.StringSetConverter;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
--- core/src/main/java/com/picus/core/domain/user/entity/User.java
+++ core/src/main/java/com/picus/core/domain/user/entity/User.java
@@ -5,13 +5,26 @@ import com.picus.core.domain.user.entity.withdrawal.Withdrawal;
 import com.picus.core.global.common.BaseEntity;
 import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
+@Getter
 @Table(name = "users")
 @SecondaryTable(
         name = "withdrawal",
         pkJoinColumns = @PrimaryKeyJoinColumn(name = "user_no")
 )
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class User extends BaseEntity {
 
     @Id @Tsid
@@ -28,4 +41,5 @@ public class User extends BaseEntity {
     })
     @Embedded
     private Withdrawal withdrawal;
+
 }
