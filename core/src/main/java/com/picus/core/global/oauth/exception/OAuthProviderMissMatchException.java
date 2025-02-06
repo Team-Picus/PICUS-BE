a0e03832079d2377750bd30196d64d4034a80176
koungq <guu119@naver.com>
2025-02-07 00:21:59 +0900

    feat: 카카오 소셜 로그인

---
+++ core/src/main/java/com/picus/core/global/oauth/exception/OAuthProviderMissMatchException.java
@@ -0,0 +1,8 @@
+package com.picus.core.global.oauth.exception;
+
+public class OAuthProviderMissMatchException extends RuntimeException {
+
+    public OAuthProviderMissMatchException(String message) {
+        super(message);
+    }
+}
