22c0ba5b871289b3b0cb01fdf9dbdeb4d0e93f93
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-02-09 13:50:44 +0900

    fix: 의존성 수정

--- core/src/main/java/com/picus/core/global/oauth/handler/TokenAccessDeniedHandler.java
+++ core/src/main/java/com/picus/core/global/oauth/handler/TokenAccessDeniedHandler.java
@@ -1,13 +1,13 @@
 package com.picus.core.global.oauth.handler;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
