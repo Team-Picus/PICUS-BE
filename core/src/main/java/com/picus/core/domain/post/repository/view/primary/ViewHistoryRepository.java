0296b8da775a13ed5b03b0e99274cdb49ab38a60
koungq <guu119@naver.com>
2025-02-16 16:01:38 +0900

    refactor: view history 엔티티화

--- core/src/main/java/com/picus/core/domain/post/repository/view/primary/ViewHistoryRepository.java
+++ core/src/main/java/com/picus/core/domain/post/repository/view/primary/ViewHistoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ViewHistoryRepository extends CrudRepository<ViewHistory, String>, ViewHistoryCustomRepository {
+public interface ViewHistoryRepository extends CrudRepository<ViewHistory, String> {
 }
