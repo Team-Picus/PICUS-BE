e93ead24aad7cd357ffe02c103ae6eecdaf23826
koungq <guu119@naver.com>
2025-02-25 02:12:04 +0900

    feat: 채팅 서비스

---
+++ core/src/main/java/com/picus/core/domain/chat/application/dto/request/SendMsgReq.java
@@ -0,0 +1,5 @@
+package com.picus.core.domain.chat.application.dto.request;
+
+public record SendMsgReq (
+        String content
+) {}
---
+++ core/src/main/java/com/picus/core/domain/chat/ui/MessageController.java
@@ -0,0 +1,24 @@
+package com.picus.core.domain.chat.ui;
+
+import com.picus.core.domain.chat.application.dto.request.SendMsgReq;
+import com.picus.core.domain.chat.application.dto.response.ReadMsgRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat/{chattingRoomNo}")
+public class MessageController {
+
+    @PostMapping("/message")
+    public void sendMessage(@PathVariable Long chattingRoomNo, @RequestBody SendMsgReq dto) {
+
+    }
+
+    @PatchMapping("/message/read")
+    public Page<ReadMsgRes> readMessages(@PathVariable Long chattingRoomNo) {
+        // 이전까지 모든 채팅들 중 "isRead = false"인 row 모두 true로 변환
+        return null;
+    }
+}
