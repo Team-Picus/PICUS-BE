409ff531432027030c9a4cba4016b8f32e9661fd
koungq <guu119@naver.com>
2025-03-26 11:12:07 +0900

    feat: 회원가입

--- core/src/main/java/com/picus/core/domain/client/application/usecase/ClientInfoUseCase.java
+++ core/src/main/java/com/picus/core/domain/client/application/usecase/ClientInfoUseCase.java
@@ -9,11 +9,15 @@ import com.picus.core.domain.shared.area.domain.entity.District;
 import com.picus.core.domain.shared.area.domain.service.AreaSearchService;
 import com.picus.core.domain.user.domain.entity.User;
 import com.picus.core.domain.user.domain.service.UserService;
+import com.picus.core.global.common.exception.RestApiException;
+import com.picus.core.global.utils.regex.BadWordFilterUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.picus.core.global.common.exception.code.status.GlobalErrorStatus._CONTAIN_BAD_WORD;
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +27,14 @@ public class ClientInfoUseCase {
     private final ClientService clientService;
     private final ClientDistrictService clientDistrictService;
     private final UserService userService;
+    private final BadWordFilterUtil badWordFilterUtil;
 
     @Transactional
     public Client save(Long userNo, SignUpReq request) {
+        if (badWordFilterUtil.isBadWord(request.nickname())) {
+            throw new RestApiException(_CONTAIN_BAD_WORD);
+        }
+
         Client client = clientService.save(userNo);
         User user = userService.findById(userNo);
         user.updateProfile(request.nickname(), request.profileImgId());
