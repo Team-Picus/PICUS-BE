f6a381d0b9abfb8255f2b5766965e3e595080a05
koungq <guu119@naver.com>
2025-03-27 13:50:09 +0900

    feat: 동시성 이슈

--- core/src/main/java/com/picus/core/CoreApplication.java
+++ core/src/main/java/com/picus/core/CoreApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableRetry
 @EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
