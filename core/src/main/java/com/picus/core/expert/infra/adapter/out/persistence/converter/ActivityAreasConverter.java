a7121a368721dde2397f14ff73104962f4264835
jhk01007 <jhk001007@naver.com>
2025-07-05 16:56:57 +0900

    test: ActivityAreasConverter 테스트 작성

---
+++ core/src/main/java/com/picus/core/expert/infra/adapter/out/persistence/converter/ActivityAreasConverter.java
@@ -0,0 +1,34 @@
+package com.picus.core.expert.infra.adapter.out.persistence.converter;
+
+import com.amazonaws.services.dynamodbv2.xspec.S;
+import com.picus.core.expert.domain.model.vo.ActivityArea;
+import jakarta.persistence.AttributeConverter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ActivityAreasConverter implements AttributeConverter<List<ActivityArea>, String> {
+    private static final String SEPARATOR = ",";
+
+    @Override
+    public String convertToDatabaseColumn(List<ActivityArea> activityAreas) {
+        if (activityAreas == null || activityAreas.isEmpty()) {
+            return "";
+        }
+        return activityAreas.stream()
+                .map(ActivityArea::name)
+                .collect(Collectors.joining(SEPARATOR));
+    }
+
+    @Override
+    public List<ActivityArea> convertToEntityAttribute(String activityAreaString) {
+        if (activityAreaString == null || activityAreaString.isEmpty()) {
+            return List.of();
+        }
+        return Arrays.stream(activityAreaString.split(SEPARATOR))
+                .map(ActivityArea::valueOf)
+                .collect(Collectors.toList());
+    }
+}
---
+++ core/src/test/java/com/picus/core/expert/infra/adapter/out/persistence/converter/ActivityAreasConverterTest.java
@@ -0,0 +1,46 @@
+package com.picus.core.expert.infra.adapter.out.persistence.converter;
+
+import com.picus.core.expert.domain.model.vo.ActivityArea;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.picus.core.expert.domain.model.vo.ActivityArea.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityAreasConverterTest {
+
+    @Test
+    @DisplayName("ActivityArea 리스트를 ,를 기준으로 하나의 문자열로 이어붙인다.")
+    public void convertToDatabaseColumn() throws Exception {
+        // given
+        ActivityAreasConverter activityAreasConverter = new ActivityAreasConverter();
+        List<ActivityArea> activityAreas = List.of(SEOUL_GANGBUKGU, SEOUL_GANGNAMGU, SEOUL_GANGSEOGU);
+
+        // when
+        String converted = activityAreasConverter.convertToDatabaseColumn(activityAreas);
+
+        // then
+        assertThat(converted)
+                .isEqualTo("SEOUL_GANGBUKGU,SEOUL_GANGNAMGU,SEOUL_GANGSEOGU");
+    }
+
+    @Test
+    @DisplayName(",를 기준으로 붙어져 있는 ActivityArea 문자열을 리스트로 변환한다.")
+    public void convertToEntityAttribute() throws Exception {
+        // given
+        ActivityAreasConverter activityAreasConverter = new ActivityAreasConverter();
+        String activityAreaString = "SEOUL_GANGBUKGU,SEOUL_GANGNAMGU,SEOUL_GANGSEOGU";
+
+        // when
+        List<ActivityArea> activityAreas = activityAreasConverter.convertToEntityAttribute(activityAreaString);
+
+        // then
+        assertThat(activityAreas).hasSize(3)
+                .containsExactly(SEOUL_GANGBUKGU, SEOUL_GANGNAMGU, SEOUL_GANGSEOGU);
+    }
+
+}
