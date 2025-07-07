ce6f89088a682aa2a47173635e2765f88d082418
jhk01007 <jhk001007@naver.com>
2025-07-07 20:20:28 +0900

    refactor: post Entity 연관관계 매핑 설정

--- core/src/main/java/com/picus/core/post/infra/adapter/out/persistence/entity/CommentEntity.java
+++ core/src/main/java/com/picus/core/post/infra/adapter/out/persistence/entity/CommentEntity.java
@@ -1,16 +1,26 @@
 package com.picus.core.post.infra.adapter.out.persistence.entity;
 
+import com.picus.core.user.infra.adapter.out.persistence.entity.UserEntity;
 import io.hypersistence.utils.hibernate.id.Tsid;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentEntity {
 
     @Id @Tsid
     private String commentNo;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_no")
+    private PostEntity postEntity;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_no")
+    private UserEntity userEntity;
+
+    @Column(nullable = false)
     private String content;
 }
