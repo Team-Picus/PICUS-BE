82361938dd940b2c7c7b2676d432adce6a69fdbc
jhk01007 <jhk001007@naver.com>
2025-07-07 19:59:15 +0900

    refactor: price Entity 연관관계 매핑

--- core/src/main/java/com/picus/core/price/infra/adapter/out/persistence/entity/PackageEntity.java
+++ core/src/main/java/com/picus/core/price/infra/adapter/out/persistence/entity/PackageEntity.java
@@ -1,22 +1,26 @@
 package com.picus.core.price.infra.adapter.out.persistence.entity;
 
 import com.picus.core.expert.infra.adapter.out.persistence.converter.StringConverter;
+import com.picus.core.expert.infra.adapter.out.persistence.entity.ThemeEntity;
 import io.hypersistence.utils.hibernate.id.Tsid;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "packages")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PackageEntity {
 
     @Id @Tsid
     private String packageNo;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_no", nullable = false)
+    private ThemeEntity themeEntity;
     private String name;
     private Integer price;
     @Convert(converter = StringConverter.class)
