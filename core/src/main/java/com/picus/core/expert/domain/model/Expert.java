0597531b384216027e55f71f606c3890b1009197
jhk01007 <jhk001007@naver.com>
2025-07-14 01:09:27 +0900

    feat: Activity Duration 계산 로직 추가

--- core/src/main/java/com/picus/core/expert/domain/model/Expert.java
+++ core/src/main/java/com/picus/core/expert/domain/model/Expert.java
@@ -1,10 +1,13 @@
 package com.picus.core.expert.domain.model;
 
+import com.amazonaws.services.kafka.model.PublicAccess;
 import com.picus.core.expert.domain.model.vo.*;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 
 @Getter
@@ -44,5 +47,23 @@ public class Expert {
 
     public void bindExpertNo(String expertNo) {
         this.expertNo = expertNo;
+    }
+
+    public void calculateActivityDuration(LocalDate now) {
+        if (this.createdAt == null) return;
+
+        LocalDate createDate = createdAt.toLocalDate();
+        Period period = Period.between(createDate, now);
+
+        String activityDuration;
+        if (period.getYears() >= 1) {
+            activityDuration = period.getYears() + "년";
+        } else if (period.getMonths() >= 1) {
+            activityDuration = period.getMonths() + "개월";
+        } else {
+            activityDuration = period.getDays() + "일";
+        }
+
+        this.activityDuration = activityDuration;
     }
 }
