ccfde1b02e926d331ef44d5c45c3c08456cbebd0
jhk01007 <jhk001007@naver.com>
2025-07-14 01:09:36 +0900

    test: Activity Duration 계산 로직 테스트 추가

---
+++ core/src/test/java/com/picus/core/expert/domain/model/ExpertTest.java
@@ -0,0 +1,60 @@
+package com.picus.core.expert.domain.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpertTest {
+
+    @Test
+    @DisplayName("ActivityDuration은 생성된지 1개월 미만이면 일단위로 설정된다.")
+    public void calculateActivityDuration_lessThanOneMonth() throws Exception {
+        // given
+        Expert expert = Expert.builder()
+                .createdAt(LocalDateTime.of(2025, 6, 14, 15, 33))
+                .build();
+
+        // when
+        expert.calculateActivityDuration(LocalDate.of(2025, 7, 13));
+
+        // then
+        assertThat(expert.getActivityDuration()).isEqualTo("29일");
+    }
+
+    @Test
+    @DisplayName("ActivityDuration은 생성된지 1개월 이상이면 월단위로 설정된다.")
+    public void calculateActivityDuration_moreThanOneMonth() throws Exception {
+        // given
+        Expert expert = Expert.builder()
+                .createdAt(LocalDateTime.of(2025, 6, 13, 15, 33))
+                .build();
+
+        // when
+        expert.calculateActivityDuration(LocalDate.of(2025, 7, 13));
+
+        // then
+        assertThat(expert.getActivityDuration()).isEqualTo("1개월");
+    }
+
+    @Test
+    @DisplayName("ActivityDuration은 생성된지 1년 이상이면 년단위로 설정된다.")
+    public void calculateActivityDuration_moreThanOneYear() throws Exception {
+        // given
+        Expert expert = Expert.builder()
+                .createdAt(LocalDateTime.of(2024, 7, 13, 15, 33))
+                .build();
+
+        // when
+        expert.calculateActivityDuration(LocalDate.of(2025, 7, 13));
+
+        // then
+        assertThat(expert.getActivityDuration()).isEqualTo("1년");
+    }
+
+}
