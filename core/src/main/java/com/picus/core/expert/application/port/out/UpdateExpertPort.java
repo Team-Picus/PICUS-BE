97a843a84683615a1d3f241b94c8c20553d79fff
jhk01007 <jhk001007@naver.com>
2025-07-14 20:08:39 +0900

    feat: 요청 승인 웹 어댑터 구현

--- core/src/main/java/com/picus/core/expert/application/port/out/UpdateExpertPort.java
+++ core/src/main/java/com/picus/core/expert/application/port/out/UpdateExpertPort.java
@@ -2,6 +2,9 @@ package com.picus.core.expert.application.port.out;
 
 import com.picus.core.expert.domain.model.Expert;
 
+/**
+ * Expert를 수정하는 Out Port
+ */
 public interface UpdateExpertPort {
 
     void updateExpert(Expert expert);
