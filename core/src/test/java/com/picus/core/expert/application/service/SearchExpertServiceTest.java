60b8275f135537dd2d034333c40ec8f202b91f03
jhk01007 <jhk001007@naver.com>
2025-07-17 06:22:02 +0900

    test: 전문가 특정 키워드로 검색 유스케이스 테스트

---
+++ core/src/test/java/com/picus/core/expert/application/service/SearchExpertServiceTest.java
@@ -0,0 +1,56 @@
+package com.picus.core.expert.application.service;
+
+import com.picus.core.expert.application.port.in.response.SearchExpertResponse;
+import com.picus.core.expert.application.port.out.LoadExpertPort;
+import com.picus.core.user.application.port.out.UserQueryPort;
+import com.picus.core.user.domain.model.ProfileImage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+
+class SearchExpertServiceTest {
+
+    private final LoadExpertPort loadExpertPort = Mockito.mock(LoadExpertPort.class);
+    private final UserQueryPort userQueryPort = Mockito.mock(UserQueryPort.class);
+    private final SearchExpertService searchExpertService = new SearchExpertService(loadExpertPort, userQueryPort);
+
+    @Test
+    @DisplayName("특정 키워드가 포함된 닉네임을 가진 전문가 찾기 서비스 메서드의 리턴값 및 상호작용 검증")
+    public void searchExpert_success() throws Exception {
+        // given
+        List<SearchExpertResponse> sampleResponses = List.of(
+                SearchExpertResponse.builder()
+                        .expertNo("test_expert_no")
+                        .nickname("test_nickname")
+                        .build()
+        );
+        BDDMockito.given(loadExpertPort.findByNicknameContaining(any(String.class)))
+                .willReturn(sampleResponses);
+        BDDMockito.given(userQueryPort.findProfileImageByExpertNo(any(String.class)))
+                .willReturn(any(ProfileImage.class));
+
+        // when
+        List<SearchExpertResponse> results = searchExpertService.searchExpert("any_keyword");
+
+        // then
+        assertThat(results).hasSize(1)
+                .extracting("expertNo", "nickname")
+                .containsExactlyInAnyOrder(tuple("test_expert_no", "test_nickname"));
+
+        then(loadExpertPort).should()
+                .findByNicknameContaining(any(String.class));
+        then(userQueryPort).should()
+                .findProfileImageByExpertNo(any(String.class));
+    }
+
+}
