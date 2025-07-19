c5cf5c5a823c551e33bcf035deddf3992297735b
KoungQ <koungq@KoungQui-MacBookAir.local>
2025-07-19 16:31:03 +0900

    refactor: 인가 로직 수정

--- core/src/main/java/com/picus/core/user/domain/model/Role.java
+++ core/src/main/java/com/picus/core/user/domain/model/Role.java
@@ -1,6 +1,15 @@
 package com.picus.core.user.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Role {
-    CLIENT,
-    EXPERT
+    CLIENT("ROLE_CLIENT"),
+    EXPERT("ROLE_EXPERT");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
 }
