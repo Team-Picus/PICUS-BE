c1ae17f9e37584f51fa97771a6406965a9dba227
KoungQ <koungq@KoungQui-MacBookAir.local>
2025-07-25 15:56:39 +0900

    feat: 무드보드 조회

--- core/src/main/java/com/picus/core/moodboard/adapter/in/web/data/response/GetMoodboardResponse.java
+++ core/src/main/java/com/picus/core/moodboard/adapter/in/web/data/response/GetMoodboardResponse.java
@@ -1,7 +1,12 @@
 package com.picus.core.moodboard.adapter.in.web.data.response;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
 public record GetMoodboardResponse(
         String userNo,
         String postNo,
-        String createdAt
+        LocalDateTime createdAt
 ) {}
--- core/src/main/java/com/picus/core/moodboard/adapter/in/web/mapper/MoodboardWebMapper.java
+++ core/src/main/java/com/picus/core/moodboard/adapter/in/web/mapper/MoodboardWebMapper.java
@@ -1,7 +1,17 @@
 package com.picus.core.moodboard.adapter.in.web.mapper;
 
+import com.picus.core.moodboard.adapter.in.web.data.response.GetMoodboardResponse;
+import com.picus.core.moodboard.domain.model.Moodboard;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MoodboardWebMapper { // DTO <-> Domain Model
+public class MoodboardWebMapper {
+
+    public GetMoodboardResponse toResponse(Moodboard moodboard) {
+        return GetMoodboardResponse.builder()
+                .userNo(moodboard.getUserNo())
+                .postNo(moodboard.getPostNo())
+                .createdAt(moodboard.getCreatedAt())
+                .build();
+    }
 }
