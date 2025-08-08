6b2cbba1f5e72f9a39c3bc8cd61c199941ca9762
KoungQ <guu119@naver.com>
2025-08-08 21:05:53 +0900

    feat: 예약 요청 API

--- core/src/main/java/com/picus/core/reservation/adapter/in/SendReservationRequestController.java
+++ core/src/main/java/com/picus/core/reservation/adapter/in/SendReservationRequestController.java
@@ -1,8 +1,8 @@
 package com.picus.core.reservation.adapter.in;
 
 import com.picus.core.reservation.adapter.in.web.data.request.SendReservationRequest;
-import com.picus.core.reservation.application.port.in.SendReservationRequestUseCase;
 import com.picus.core.reservation.adapter.in.web.mapper.SendReservationWebMapper;
+import com.picus.core.reservation.application.port.in.SendReservationRequestUseCase;
 import com.picus.core.reservation.application.port.in.request.SaveReservationCommand;
 import com.picus.core.shared.annotation.CurrentUser;
 import com.picus.core.shared.common.BaseResponse;
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +20,10 @@ public class SendReservationRequestController {
     private final SendReservationWebMapper webMapper;
 
     @PostMapping("/api/v1/reservation/request")
-    public BaseResponse<Void> sendRequest(@CurrentUser String userNo, @RequestBody @Valid SendReservationRequest request) {
+    public BaseResponse<Void> sendRequest(
+            @CurrentUser String userNo,
+            @RequestBody @Valid SendReservationRequest request
+    ) {
         SaveReservationCommand command = webMapper.toCommand(request);
         sendReservationRequestUseCase.send(userNo, command);
         return BaseResponse.onSuccess();
